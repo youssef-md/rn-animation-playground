134a7839b3841e45d34094c0f6c81033db9667f2
youssef-md <emaildeyoussefmuhamad@gmail.com>
2020-08-31 00:00:58 -0300

    Move card using PanGestureHandler and default Animated API

--- src/screens/TinderCards/index.tsx
+++ src/screens/TinderCards/index.tsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { AntDesign, Entypo } from '@expo/vector-icons';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import { Animated } from 'react-native';
 
 import { Container, Card, CardImage, Footer, RoundButton } from './styles';
 import { movies } from './data';
 
 const TinderCards: React.FC = () => {
+  const translate = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationX: translate.x,
+          translationY: translate.y,
+        },
+      },
+    ],
+    { useNativeDriver: true },
+  );
+
+  function onHandlerStateChanged(event) {}
+
   return (
     <Container>
-      <Card>
-        <CardImage source={movies[0].poster} resizeMode="cover" />
-      </Card>
+      <PanGestureHandler
+        onGestureEvent={animatedEvent}
+        onHandlerStateChange={onHandlerStateChanged}>
+        <Card
+          as={Animated.View}
+          style={{
+            transform: [
+              { translateX: translate.x },
+              { translateY: translate.y },
+            ],
+          }}>
+          <CardImage source={movies[0].poster} resizeMode="cover" />
+        </Card>
+      </PanGestureHandler>
       <Footer>
         <RoundButton>
           <Entypo name="cross" size={45} color="#f57676" />
