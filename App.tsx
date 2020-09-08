ebaa84807aef567088feabfb35a685d3445269b8
youssef-md <emaildeyoussefmuhamad@gmail.com>
2020-09-07 21:51:56 -0300

    Config app.json to build version with splash and icon

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
-import React, { useState, useCallback } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { ThemeProvider } from "styled-components";
+import React, { useState, useCallback } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components';
 
-import Routes from "./src/routes";
-import AppProvider from "./src/hooks";
-import { themes } from "./src/styles";
+import Routes from './src/routes';
+import AppProvider from './src/hooks';
+import { themes } from './src/styles';
 
 export default function App() {
-  const [theme, setTheme] = useState(themes.dark);
+  const [theme, setTheme] = useState(themes.light);
 
   const switchTheme = useCallback(() => {
-    setTheme(theme.type === "dark" ? themes.light : themes.dark);
+    setTheme(theme.type === 'dark' ? themes.light : themes.dark);
   }, [theme.type]);
 
   return (
