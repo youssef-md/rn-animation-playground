684da9e54b2e67ee29e9a83f4dc97c6039fe396c
youssef-md <emaildeyoussefmuhamad@gmail.com>
2020-10-30 22:35:37 -0300

    Listen to fling up and down and handle reactive animation

--- src/screens/PlacesTransition/index.tsx
+++ src/screens/PlacesTransition/index.tsx
@@ -1,8 +1,13 @@
 // Inspiration: https://dribbble.com/shots/3731362-Event-cards-iOS-interaction
 
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { TouchableOpacity, FlatList, View } from 'react-native';
+import { TouchableOpacity, FlatList, View, Animated } from 'react-native';
+import {
+  Directions,
+  FlingGestureHandler,
+  State,
+} from 'react-native-gesture-handler';
 
 import {
   Container,
@@ -19,47 +24,95 @@ import { places } from './data';
 const SharedTransition: React.FC = () => {
   const navigation = useNavigation();
 
+  const [activeIndex, setActiveIndex] = useState(0);
+  const animatedValue = useRef(new Animated.Value(0)).current;
+  const reactiveAnimated = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(animatedValue, {
+      toValue: reactiveAnimated,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  }, [animatedValue, reactiveAnimated]);
+
+  const setActiveSlide = useCallback(
+    (newIndex) => {
+      setActiveIndex(newIndex);
+      reactiveAnimated.setValue(newIndex);
+    },
+    [reactiveAnimated],
+  );
+
   return (
-    <Container>
-      {/* Select a Place Title */}
+    <FlingGestureHandler
+      key="UP"
+      direction={Directions.UP}
+      onHandlerStateChange={(ev) => {
+        if (
+          ev.nativeEvent.state === State.END &&
+          activeIndex !== places.length - 1
+        ) {
+          setActiveSlide(activeIndex + 1);
+        }
+      }}>
+      <FlingGestureHandler
+        key="DOWN"
+        direction={Directions.DOWN}
+        onHandlerStateChange={(ev) => {
+          if (ev.nativeEvent.state === State.END && activeIndex !== 0) {
+            setActiveSlide(activeIndex - 1);
+          }
+        }}>
+        <Container>
+          {/* Select a Place Title */}
 
-      <FlatList
-        data={places}
-        keyExtractor={({ title }) => title}
-        contentContainerStyle={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-        CellRendererComponent={({ index, item, children, style, ...props }) => {
-          const newStyle = [
-            style,
-            {
-              zIndex: places.length - index,
-              left: -ITEM_WIDTH / 2,
-              top: -ITEM_HEIGHT / 2 - 20,
-            },
-          ];
+          <FlatList
+            data={places}
+            keyExtractor={({ name }) => name}
+            scrollEnabled={false}
+            contentContainerStyle={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            CellRendererComponent={({
+              index,
+              item,
+              children,
+              style,
+              ...props
+            }) => {
+              const newStyle = [
+                style,
+                {
+                  zIndex: places.length - index,
+                  left: -ITEM_WIDTH / 2,
+                  top: -ITEM_HEIGHT / 2 - 20,
+                },
+              ];
 
-          return (
-            <View index={index} style={newStyle} {...props}>
-              {children}
-            </View>
-          );
-        }}
-        renderItem={({ item }) => {
-          const { poster, name } = item;
-          return (
-            <PlaceContainer>
-              <TouchableOpacity>
-                <PlaceImage source={poster} resizeMode="cover" />
-                <PlaceName>{name}</PlaceName>
-              </TouchableOpacity>
-            </PlaceContainer>
-          );
-        }}
-      />
-    </Container>
+              return (
+                <View index={index} style={newStyle} {...props}>
+                  {children}
+                </View>
+              );
+            }}
+            renderItem={({ item }) => {
+              const { poster, name } = item;
+              return (
+                <PlaceContainer>
+                  <TouchableOpacity>
+                    <PlaceImage source={poster} resizeMode="cover" />
+                    <PlaceName>{name}</PlaceName>
+                  </TouchableOpacity>
+                </PlaceContainer>
+              );
+            }}
+          />
+        </Container>
+      </FlingGestureHandler>
+    </FlingGestureHandler>
   );
 };
 
